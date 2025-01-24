0d7e05e6888216ae8ff147733b420a3581074ddc
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-01-24 00:33:40 -0600

    Update app.routes.ts

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CatalogListComponent } from './catalog-list/catalog-list.component';
 import { CatalogViewComponent } from './catalog-view/catalog-view.component';
 import { CatalogEditComponent } from './catalog-edit/catalog-edit.component';
+import { OrderSearchComponent } from './order-search/order-search.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'catalog', pathMatch: 'full' },
   { path: 'catalog', component: CatalogListComponent },
+  { path: 'order', component: OrderSearchComponent },
   { path: 'catalog/view/:id', component: CatalogViewComponent },
   { path: 'catalog/edit/:id', component: CatalogEditComponent },
  ];
