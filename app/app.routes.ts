bb59c674d6fe3d3d3d265a62db40edadf4394bd1
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-01-23 22:29:19 -0600

    Update app.routes.ts

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { CatalogListComponent } from './catalog-list/catalog-list.component';
 import { CatalogViewComponent } from './catalog-view/catalog-view.component';
 import { CatalogEditComponent } from './catalog-edit/catalog-edit.component';
-import { CatalogDeleteComponent } from './catalog-delete/catalog-delete.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'catalog', pathMatch: 'full' },
   { path: 'catalog', component: CatalogListComponent },
   { path: 'catalog/view/:id', component: CatalogViewComponent },
   { path: 'catalog/edit/:id', component: CatalogEditComponent },
-  { path: 'catalog/delete/:id', component: CatalogDeleteComponent },
-];
+ ];
