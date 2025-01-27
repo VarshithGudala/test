7c86e41981ac4a037ace60dce7d2c3096ff834ed
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-01-27 08:19:44 -0600

    Add files via upload

--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  selector: 'header',
-  template: `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Catalog App</a>
-  </nav>`,
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
