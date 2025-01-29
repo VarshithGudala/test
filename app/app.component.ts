af1d2c9a7afad2000dbad39a48ca0ff159a815d1
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-01-29 08:41:04 -0600

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { CustomerSearchComponent } from './customer-search/customer-search.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  template: `
-    <app-header></app-header>
-    <div class="container mt-4">
-      <app-customer-search></app-customer-search>
-    </div>
-    <app-footer></app-footer>
-  `,
-  imports: [
-    RouterModule,
-    HeaderComponent,
-    FooterComponent,
-    CustomerSearchComponent
-  ]
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'coo-ui-app';
+}
