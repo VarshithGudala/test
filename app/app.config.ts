15f520726393950f17ca90847581fdf45cf462d9
VarshithGudala <84899041+VarshithGudala@users.noreply.github.com>
2025-02-05 07:35:53 -0600

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,50 @@
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MsalModule, MsalService, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalInterceptorConfiguration, MsalRedirectComponent, MsalGuard, MsalBroadcastService, MsalInterceptor } from '@azure/msal-angular';
+import { PublicClientApplication, InteractionType } from '@azure/msal-browser';
+import { msalConfig } from './auth-config';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { appRoutes } from './app.routes';
+
+export function MSALInstanceFactory() {
+  return new PublicClientApplication(msalConfig);
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect
+  };
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap: new Map([['https://graph.microsoft.com/v1.0/me', ['user.read']]])
+  };
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+    provideRouter([], withComponentInputBinding()),
+    provideHttpClient(withInterceptorsFromDi()),
+    MsalModule,
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+    MsalInterceptor
+  ]
 };
